a47bb5c5a53f90ccb316aec11755ee1f567303fb
Josh Comer <joshjcomer@gmail.com>
2012-01-28 15:23:54 -0400

    Added the deterministic algorithm, time to do some analysis

--- src/median/core.clj
+++ src/median/core.clj
@@ -1,5 +1,5 @@
 (ns median.core
-  (:use [clojure.math.numeric-tower :only [floor]]))
+  (:use [clojure.math.numeric-tower :only [floor ceil]]))
 
 (defn rand-list
   "Generates a random vector of length n with unique
@@ -7,14 +7,14 @@
   [n]
   (shuffle (map inc (range n))))
 
-(defn rand-median
-  "A randomized median finding algorithm"
-  [l]
+(defn- median-skeleton
+  "The skeleton for the median finding algorithms"
+  [l pivot-fn]
   (loop [l l
          median (floor (/ (count l) 2))]
     (if (= 1 (count l))
       (first l)
-      (let [pivot (nth l (rand-int (count l)))
+      (let [pivot (pivot-fn l)
             less-part (filter #(< % pivot) l)
             great-part (filter #(> % pivot) l)
             rank (inc (count less-part))]
@@ -24,3 +24,25 @@
                                 median)
          :else (recur great-part
                       (- median rank)))))))
+
+(defn rand-median
+  "A randomized median finding algorithm"
+  [l]
+  (median-skeleton l #(floor (/ (count %) 2))))
+
+(declare deter-median)
+
+(defn find-median-of-medians
+  [l]
+  (let [l (into [] l)]
+    (if (< (count l) 5)
+     (nth (into [] (sort l)) (ceil (/ (count l) 2)))
+     (let [subvecs (partition 5
+                              (subvec l 0 (- (count l) (mod (count l) 5))))
+           medians (map (fn [n] (nth (sort n) 2)) subvecs)]
+       (deter-median (into [] medians))))))
+
+(defn deter-median
+  "A median finding algorithm with asymptotically linear running time"
+  [l]
+  (median-skeleton l find-median-of-medians))
